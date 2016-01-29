a2300906656ee61cd5568fb8c3b3007c1ef9e2b6
Camilo Sepulveda <kmilo.2506@gmail.com>
2016-01-29 01:41:45 +0000

    Add documentation

--- app/src/main/java/com/kmilo2506/annotationssample/activities/FormActivity.java
+++ app/src/main/java/com/kmilo2506/annotationssample/activities/FormActivity.java
@@ -7,14 +7,29 @@ import com.kmilo2506.annotationssample.fragments.FormFragment_;
 import org.androidannotations.annotations.EActivity;
 
 /**
- * Created by kmilo2506 on 1/27/16.
+ * Created by Camilo Sepulveda on 1/27/16.
  */
 
+/* @EActivity annotation is necessary so you can use the
+ * other annotations in your activities, fragments etc.
+ */
 @EActivity
 public class FormActivity extends SingleFragmentActivity {
 
+    /* Here you return a new FormFragment so SingleFragmentActivity 
+     * can add it to the FragmentManager and present it when the
+     * activity is created.
+     */
     @Override
     protected Fragment createFragment() {
+        /* Notice the underscore at the end of the fragment's name.
+         * When using the @EActivity or @EFragment annotations, 
+         * the framework creates new classes at compile time with 
+         * the same name as your fragments and activities but with
+         * an underscore at the end. This is why when you need to 
+         * reference this activities or fragments you must put an
+         * underscore at the end of their names.
+         */
         return new FormFragment_();
     }
 }
